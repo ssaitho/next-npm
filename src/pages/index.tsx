55cccfffa3bb4060a6603e82e7c4c2d04a8ccdad
Shinsuke Saito <s.s.saito108@gmail.com>
2022-09-28 20:19:36 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 import type { NextPage } from "next";
 // import { Button } from "@ssaitho/react_ts_storybook";
+import { Button } from "@openameba/spindle-ui";
+import { PrimaryButton, StatusLabel } from "smarthr-ui";
 
 const Home: NextPage = () => {
   return (
     <div>
       <p>home</p>
+      <Button size="large" variant="contained">
+        Spindle
+      </Button>
+      <PrimaryButton>Hello World</PrimaryButton>
+      <StatusLabel type="red">ラベル名</StatusLabel>
     </div>
   );
 };
