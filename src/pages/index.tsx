e09ebc510b84ccc05a2f6a8ec7dad338ff718ec6
s-saito <s.s.saito108@gmail.com>
2022-09-30 00:43:14 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 import type { NextPage } from "next";
-// import { Button } from "@ssaitho/react_ts_storybook";
-import { Button } from "@openameba/spindle-ui";
+import { Button } from "@ssaitho/react_ts_storybook";
+// import { Button } from "@openameba/spindle-ui";
 import { PrimaryButton, StatusLabel } from "smarthr-ui";
 
 const Home: NextPage = () => {
   return (
     <div>
       <p>home</p>
-      <Button size="large" variant="contained">
+      <Button size="large">Spindle</Button>
+      {/* <Button size="large" variant="contained">
         Spindle
-      </Button>
+      </Button> */}
       <PrimaryButton>Hello World</PrimaryButton>
       <StatusLabel type="red">ラベル名</StatusLabel>
     </div>
